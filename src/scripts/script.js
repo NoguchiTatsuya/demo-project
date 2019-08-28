186197514c07e78db3c0712dedf2cf0b0f78fe11
NoguchiTatsuya <sao.rain.endo@gmail.com>
2019-08-28 11:45:58 +0700

    commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -15,11 +15,11 @@ $(document).ready(function(){
 });
 
 $(function() {
- var flag=0;
   // ナビゲーションのリンクを指定
  var navLink = $('#gnav div li a');
  var navLink1 = $('#main-header div li a');
  var flag = false;
+ var flag1 = 0;
  var pagetop = $('.fixed-real');
   $(window).scroll(function () {
     if ($(this).scrollTop() > 500) {
@@ -59,6 +59,18 @@ $(function() {
   function currentCheck() {
      // 現在のスクロール位置を取得
       var windowScrolltop = $(window).scrollTop();
+      if (contentsArr[1][0] <= windowScrolltop -10 && contentsArr[1][1] >= windowScrolltop && flag1==0) {
+        var num = 0;
+        var tgt = 200;
+        var speed = 10;
+        setInterval(function(){
+          if(num <= tgt){
+	          document.getElementById("count").innerText = num;
+	          num++;
+          }
+        },speed);
+        flag1=1;
+      }
       for (var i = 0; i < contentsArr.length; i++) {
          // 現在のスクロール位置が、配列に格納した開始位置と終了位置の間にあるものを調べる
         if(contentsArr[i][0] <= windowScrolltop -10 && contentsArr[i][1] >= windowScrolltop) {
@@ -73,19 +85,8 @@ $(function() {
  // ページ読み込み時とスクロール時に、現在地をチェックする
   $(window).on('load scroll', function() {
     currentCheck();
-    if (contentsArr[1][0] <= windowScrolltop -10 && contentsArr[1][1] >= windowScrolltop && flag==0) {
-      var num = 0;
-      var tgt = 200;
-      var speed = 10;
-      setInterval(function(){
-        if(num <= tgt){
-          document.getElementById("count").innerText = num;
-          num++;
-        }
-      },speed);
-      flag=1;
-    }
   });
+  
 
 // ナビゲーションをクリックした時のスムーズスクロール
   navLink.click(function() {
